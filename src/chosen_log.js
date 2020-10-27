6910a12b5d27518652040fbda33baeb6f368f2ca
Thomas Flint <tommyflint2000@gmail.com>
2020-10-27 19:42:46 +1000

    added welcome message

--- src/chosen_log.js
+++ src/chosen_log.js
@@ -20,12 +20,35 @@ export default function Chosen_Log(props){
         window.addEventListener('scroll',handleScroll)
     })
 
+    function WelcomeTutorial(){
+      if(props.list.length == 0){
+        return(
+          <div className="welcome">
+            Welcome to this Marvel 5-side Soccer Team Selector! Select your favourite Marvel characters by name below, and add them to your team!
+            Once you click on a character, their portrait will appear at the top of the screen. Click on them again to view more info, or remove them from your team.
+          </div>
+        )
+      }
+      else{
+        return(
+          <div className="nothing">
+
+          </div>
+        )
+      }
+    }
+
+    //Make modal menu that appear when you click, showing more info like position,
+    //description of character, etc
     function buttons(item, index){
       return(
         <div key={index}>
-          <button className="char-button">
+          <button className="char-button" value={index}>
             {item.name}
+            <img src={item.thumbnail.path+'.'+item.thumbnail.extension}/>
+            {item.position}
           </button>
+          
         </div>
         
       )
@@ -38,6 +61,7 @@ export default function Chosen_Log(props){
     return(
         <div className={x.join(" ")}>
             <div className="chosen-characters-element">
+              <WelcomeTutorial/>
               {props.list.map(buttons)}
             </div>
         </div>
