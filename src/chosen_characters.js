052f579c97b0fcc460e36e4055112f67cfbae144
Thomas Flint <tommyflint2000@gmail.com>
2020-10-27 14:43:10 +1000

    state and buttons

--- src/chosen_characters.js
+++ src/chosen_characters.js
@@ -3,17 +3,38 @@ import Character_Browser from './character_browser'
 
 
 export default function Chosen_Characters(GK, ST, MD, DF, flexible){
+    const [goalkeeper, setGK] = useState(null);
+    const [striker, setST] = useState(null);
+    const [midfielder, setMD] = useState(null);
+    const [defender, setDF] = useState(null);
+    const [flex, setFlex] = useState(null);
 
+    const positions = {'GK': goalkeeper,
+                        'ST': striker,
+                        'MD': midfielder,
+                        'DF': defender,
+                        '??': flex
+    }
 
-
-    function Character_Button(pc, character){
+    function Character_Button(pc){
         //Customise button to have some of the characteristics of the character
         //that it is representing
-        return(
-            <button className="char-button" name={pc['pc']}>
-                {pc['pc']}
-            </button>
-        )
+        if(positions[pc['pc']] == null){
+            return(
+                <button className="char-button" name={pc['pc']}>
+                    {pc['pc']}
+                </button>
+            )
+        }
+        else{
+            return(
+                <button className="char-button" name={pc['pc']}>
+                    <img src={positions[pc['pc']].thumbnail.path+'.'+positions[pc['pc']].thumbnail.extension} width='75' height='75'>
+                    {pc['pc']}
+                    </img>
+                </button>
+            )
+        }
     }
 
     return(
